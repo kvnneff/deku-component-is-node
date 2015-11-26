6c1ec078ba0a50d22a3f6d9babc6eb09f2d942c5
Kevin Neff <kvnneff@gmail.com>
2015-11-25 19:20:45 -0800

    Add comments.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-module.exports = function (node, type) {
+/**
+ * Returns `true` if `node` is a valid Deku node.  If `type` is specified,
+ * it will ensure that type is strictly equal. (whether that is a `Component`
+ * or a `String` element name)
+ * @param  {DekuElement}  node The virtual node to check.
+ * @param  {String|Object}  [type] If given, the type of node this must be.
+ * @return {Boolean}
+ */
+function isNode (node, type) {
   var isNode = true
   var requiredProps = [
     'attributes',
@@ -16,3 +24,5 @@ module.exports = function (node, type) {
   if (type && isNode) return node.type === type
   return isNode
 }
+
+module.exports = isNode
